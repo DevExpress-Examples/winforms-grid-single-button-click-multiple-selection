4b0a2c18fd304d3ad91e5953cc89e79ad9292d75
DevExpressExampleBot <fakeEmail>
2023-09-11 17:29:28 +0400

    Source auto update [skip ci]

--- VB/WindowsApplication168/Form1.Designer.vb
+++ VB/WindowsApplication168/Form1.Designer.vb
@@ -1,150 +1,156 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace SingleButtonClick
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
-			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			Me.dataSet1 = New System.Data.DataSet()
-			Me.dataTable1 = New System.Data.DataTable()
-			Me.dataColumn1 = New System.Data.DataColumn()
-			Me.dataColumn2 = New System.Data.DataColumn()
-			Me.bindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
-			Me.colID = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colName = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.repositoryItemComboBox1 = New DevExpress.XtraEditors.Repository.RepositoryItemComboBox()
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.dataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.dataTable1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.bindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.repositoryItemComboBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' gridControl1
-			' 
-			Me.gridControl1.DataSource = Me.bindingSource1
-			Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.gridControl1.Location = New System.Drawing.Point(0, 0)
-			Me.gridControl1.MainView = Me.gridView1
-			Me.gridControl1.Name = "gridControl1"
-			Me.gridControl1.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() { Me.repositoryItemComboBox1})
-			Me.gridControl1.Size = New System.Drawing.Size(833, 413)
-			Me.gridControl1.TabIndex = 0
-			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
-			' 
-			' gridView1
-			' 
-			Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colID, Me.colName})
-			Me.gridView1.GridControl = Me.gridControl1
-			Me.gridView1.Name = "gridView1"
-			Me.gridView1.OptionsSelection.MultiSelect = True
-			Me.gridView1.OptionsSelection.MultiSelectMode = DevExpress.XtraGrid.Views.Grid.GridMultiSelectMode.CellSelect
-'			Me.gridView1.MouseDown += New System.Windows.Forms.MouseEventHandler(Me.gridView1_MouseDown);
-			' 
-			' dataSet1
-			' 
-			Me.dataSet1.DataSetName = "NewDataSet"
-			Me.dataSet1.Tables.AddRange(New System.Data.DataTable() { Me.dataTable1})
-			' 
-			' dataTable1
-			' 
-			Me.dataTable1.Columns.AddRange(New System.Data.DataColumn() { Me.dataColumn1, Me.dataColumn2})
-			Me.dataTable1.TableName = "Table1"
-			' 
-			' dataColumn1
-			' 
-			Me.dataColumn1.ColumnName = "ID"
-			Me.dataColumn1.DataType = GetType(Integer)
-			' 
-			' dataColumn2
-			' 
-			Me.dataColumn2.ColumnName = "Name"
-			' 
-			' bindingSource1
-			' 
-			Me.bindingSource1.DataMember = "Table1"
-			Me.bindingSource1.DataSource = Me.dataSet1
-			' 
-			' colID
-			' 
-			Me.colID.Caption = "ID"
-			Me.colID.FieldName = "ID"
-			Me.colID.Name = "colID"
-			Me.colID.Visible = True
-			Me.colID.VisibleIndex = 0
-			' 
-			' colName
-			' 
-			Me.colName.Caption = "Name"
-			Me.colName.ColumnEdit = Me.repositoryItemComboBox1
-			Me.colName.FieldName = "Name"
-			Me.colName.Name = "colName"
-			Me.colName.ShowButtonMode = DevExpress.XtraGrid.Views.Base.ShowButtonModeEnum.ShowAlways
-			Me.colName.Visible = True
-			Me.colName.VisibleIndex = 1
-			' 
-			' repositoryItemComboBox1
-			' 
-			Me.repositoryItemComboBox1.AutoHeight = False
-			Me.repositoryItemComboBox1.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() { New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
-			Me.repositoryItemComboBox1.Items.AddRange(New Object() { "Item1", "Item2", "Item3"})
-			Me.repositoryItemComboBox1.Name = "repositoryItemComboBox1"
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(833, 413)
-			Me.Controls.Add(Me.gridControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-'			Me.Load += New System.EventHandler(Me.Form1_Load);
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.dataSet1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.dataTable1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.bindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.repositoryItemComboBox1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.dataSet1 = New System.Data.DataSet()
+            Me.dataTable1 = New System.Data.DataTable()
+            Me.dataColumn1 = New System.Data.DataColumn()
+            Me.dataColumn2 = New System.Data.DataColumn()
+            Me.bindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+            Me.colID = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colName = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.repositoryItemComboBox1 = New DevExpress.XtraEditors.Repository.RepositoryItemComboBox()
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.dataSet1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.dataTable1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.bindingSource1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.repositoryItemComboBox1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' gridControl1
+            ' 
+            Me.gridControl1.DataSource = Me.bindingSource1
+            Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.gridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.gridControl1.MainView = Me.gridView1
+            Me.gridControl1.Name = "gridControl1"
+            Me.gridControl1.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.repositoryItemComboBox1})
+            Me.gridControl1.Size = New System.Drawing.Size(833, 413)
+            Me.gridControl1.TabIndex = 0
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gridView1})
+            ' 
+            ' gridView1
+            ' 
+            Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.colID, Me.colName})
+            Me.gridView1.GridControl = Me.gridControl1
+            Me.gridView1.Name = "gridView1"
+            Me.gridView1.OptionsSelection.MultiSelect = True
+            Me.gridView1.OptionsSelection.MultiSelectMode = DevExpress.XtraGrid.Views.Grid.GridMultiSelectMode.CellSelect
+            AddHandler Me.gridView1.MouseDown, New System.Windows.Forms.MouseEventHandler(AddressOf Me.gridView1_MouseDown)
+            ' 
+            ' dataSet1
+            ' 
+            Me.dataSet1.DataSetName = "NewDataSet"
+            Me.dataSet1.Tables.AddRange(New System.Data.DataTable() {Me.dataTable1})
+            ' 
+            ' dataTable1
+            ' 
+            Me.dataTable1.Columns.AddRange(New System.Data.DataColumn() {Me.dataColumn1, Me.dataColumn2})
+            Me.dataTable1.TableName = "Table1"
+            ' 
+            ' dataColumn1
+            ' 
+            Me.dataColumn1.ColumnName = "ID"
+            Me.dataColumn1.DataType = GetType(Integer)
+            ' 
+            ' dataColumn2
+            ' 
+            Me.dataColumn2.ColumnName = "Name"
+            ' 
+            ' bindingSource1
+            ' 
+            Me.bindingSource1.DataMember = "Table1"
+            Me.bindingSource1.DataSource = Me.dataSet1
+            ' 
+            ' colID
+            ' 
+            Me.colID.Caption = "ID"
+            Me.colID.FieldName = "ID"
+            Me.colID.Name = "colID"
+            Me.colID.Visible = True
+            Me.colID.VisibleIndex = 0
+            ' 
+            ' colName
+            ' 
+            Me.colName.Caption = "Name"
+            Me.colName.ColumnEdit = Me.repositoryItemComboBox1
+            Me.colName.FieldName = "Name"
+            Me.colName.Name = "colName"
+            Me.colName.ShowButtonMode = DevExpress.XtraGrid.Views.Base.ShowButtonModeEnum.ShowAlways
+            Me.colName.Visible = True
+            Me.colName.VisibleIndex = 1
+            ' 
+            ' repositoryItemComboBox1
+            ' 
+            Me.repositoryItemComboBox1.AutoHeight = False
+            Me.repositoryItemComboBox1.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+            Me.repositoryItemComboBox1.Items.AddRange(New Object() {"Item1", "Item2", "Item3"})
+            Me.repositoryItemComboBox1.Name = "repositoryItemComboBox1"
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(833, 413)
+            Me.Controls.Add(Me.gridControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.dataSet1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.dataTable1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.bindingSource1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.repositoryItemComboBox1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private gridControl1 As DevExpress.XtraGrid.GridControl
-		Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
-		Private bindingSource1 As System.Windows.Forms.BindingSource
-		Private dataSet1 As System.Data.DataSet
-		Private dataTable1 As System.Data.DataTable
-		Private dataColumn1 As System.Data.DataColumn
-		Private dataColumn2 As System.Data.DataColumn
-		Private colID As DevExpress.XtraGrid.Columns.GridColumn
-		Private colName As DevExpress.XtraGrid.Columns.GridColumn
-		Private repositoryItemComboBox1 As DevExpress.XtraEditors.Repository.RepositoryItemComboBox
-	End Class
+#End Region
+        Private gridControl1 As DevExpress.XtraGrid.GridControl
+
+        Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+
+        Private bindingSource1 As System.Windows.Forms.BindingSource
+
+        Private dataSet1 As System.Data.DataSet
+
+        Private dataTable1 As System.Data.DataTable
+
+        Private dataColumn1 As System.Data.DataColumn
+
+        Private dataColumn2 As System.Data.DataColumn
+
+        Private colID As DevExpress.XtraGrid.Columns.GridColumn
+
+        Private colName As DevExpress.XtraGrid.Columns.GridColumn
+
+        Private repositoryItemComboBox1 As DevExpress.XtraEditors.Repository.RepositoryItemComboBox
+    End Class
 End Namespace
-
